6339d5bdc4957f81bb9313347fbd0a0e4b570f87
john-savepoint <john@savepoint.com.au>
2024-01-16 20:09:41 +0900

    lesson 10 - added form to demonstrate webhook

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import React from "react";
-import { createRoot } from "react-dom";
+import { createRoot } from "react-dom/client";
 import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
-const App = () => (
-  <div className="normal">
-    <h1>Adopt Me!</h1>
-    <Pet name="Luna" breed="Husky" age={10} />
-    <Pet name="Sparky" breed="Labrador" age={25} />
-    <Pet name="Doink" breed="Foxy" age={30} />
-  </div>
-);
+const App = () => {
+  return (
+    <div className="normal">
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" breed="Husky" age={10} />
+      <Pet name="Spark" breed="Labrador" age={25} />
+      <Pet name="Doink" breed="Foxy" age={30} />
+      <SearchParams />
+    </div>
+  );
+};
 
 const container = document.getElementById("root");
 const root = createRoot(container);
