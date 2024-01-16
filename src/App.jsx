d8d2e67b71b1aa3800214b435f8fd5bcce2d18f3
john-savepoint <john@savepoint.com.au>
2024-01-16 15:06:31 +0900

    changed app.jsx from traditional react to jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,39 @@
 import React from "react";
 import { createRoot } from "react-dom";
+import PropTypes from "prop-types";
 
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.breed),
-    React.createElement("h2", {}, props.age),
-  ]);
+// const Pet = ({ name, breed, age }) => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", {}, name),
+//     React.createElement("h2", {}, breed),
+//     React.createElement("h2", {}, age),
+//   ]);
+// };
+
+const Pet = ({ name, breed, age }) => {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{breed}</h2>
+      <h2>{age}</h2>
+    </div>
+  );
 };
 
-const App = () => {
-  return React.createElement("div", { className: "normal" }, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, { name: "Luna", breed: "Husky", age: "10" }),
-    React.createElement(Pet, { name: "Sparky", breed: "Labrador", age: "25" }),
-    React.createElement(Pet, { name: "DOink", breed: "FOxy", age: "30" }),
-  ]);
+Pet.propTypes = {
+  name: PropTypes.string.isRequired,
+  breed: PropTypes.string.isRequired,
+  age: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
 };
+
+const App = () => (
+  <div className="normal">
+    <h1>Adopt Me!</h1>
+    <Pet name="Luna" breed="Husky" age={10} />
+    <Pet name="Sparky" breed="Labrador" age={25} />
+    <Pet name="Doink" breed="Foxy" age={30} />
+  </div>
+);
 
 const container = document.getElementById("root");
 const root = createRoot(container);
