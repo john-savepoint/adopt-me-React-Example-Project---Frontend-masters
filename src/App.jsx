4f5c18cc3694100be005af6a9b37078ec5139146
john-savepoint <john@savepoint.com.au>
2024-02-12 01:00:57 +0900

    added React Query and wrote fetchMethod for ReactQuery

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,35 @@
 import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 // import Pet from "./Pet";
 import SearchParams from "./SearchParams";
 import { Link, BrowserRouter, Route, Routes } from "react-router-dom";
 import Details from "./Details";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
+
 const App = () => {
   return (
     <BrowserRouter>
-      <header>
-        <Link to="/"> Adopt Me!</Link>
-      </header>
-      <Routes>
-        <Route path="/details/:id" element={<Details />} />
-        <Route path="/" element={<SearchParams />} />
-      </Routes>
-      {/* <Pet name="Luna" breed="Husky" age={10} />
-      <Pet name="Spark" breed="Labrador" age={25} />
-      <Pet name="Doink" breed="Foxy" age={30} /> */}
-      {/* <SearchParams /> */}
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to="/"> Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+        {/* <Pet name="Luna" breed="Husky" age={10} />
+        <Pet name="Spark" breed="Labrador" age={25} />
+        <Pet name="Doink" breed="Foxy" age={30} /> */}
+        {/* <SearchParams /> */}
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
